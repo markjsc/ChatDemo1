1d52e29690eac88fb9f3c318ae2ce689f9fff5a2
Mark Johnston <mark.johnston@andosi.com>
2023-05-20 12:30:09 -0500

    initial working state

--- src/components/chat/index.tsx
+++ src/components/chat/index.tsx
@@ -1,10 +1,41 @@
-import React from 'react';
-import {Text, View} from 'react-native';
+import React, {useCallback, useEffect, useState} from 'react';
+import {KeyboardAvoidingView, View} from 'react-native';
+import {GiftedChat, IMessage} from 'react-native-gifted-chat';
 
 function ChatComponent(): JSX.Element {
+  const [messages, setMessages] = useState<IMessage[]>([]);
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Test',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'Admin',
+        },
+      },
+    ]);
+  }, []);
+
+  const onSend = useCallback((_messages: IMessage[] = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, _messages),
+    );
+  }, []);
+
   return (
-    <View>
-      <Text>This is the chat component</Text>
+    <View style={[{flex: 1}]}>
+      <GiftedChat
+        messages={messages}
+        onSend={(_messages: IMessage[]) => onSend(_messages)}
+        user={{
+          _id: 1,
+        }}
+        forceGetKeyboardHeight
+      />
+      <KeyboardAvoidingView behavior="padding" keyboardVerticalOffset={80} />
     </View>
   );
 }
